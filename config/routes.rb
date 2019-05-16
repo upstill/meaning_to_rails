2373d22ee3129190591ed540e7f01464371aaeb2
Steve Upstill <steve@upstill.net>
2019-05-16 16:36:57 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  use_doorkeeper
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :list_types
+      resources :list_items
+    end
+  end
   get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
+  get 'login', to: 'sessions#new', as: 'sign_in'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users
   resources :sessions
-  use_doorkeeper
   resources :list_types
   resources :list_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
